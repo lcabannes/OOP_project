62e463c2097ebfc0f2163df60207315f7407f275
loic <loic.cabannes@gmail.com>
2023-12-01 11:14:21 +0100

    create dev branch

--- src/com/li/oopproject/Game.java
+++ src/com/li/oopproject/Game.java
@@ -2,8 +2,38 @@ package com.li.oopproject;
 
 public class Game {
     private GameInterface gameInterface;
-    public void startGame(){
+    private int level;
+    private int currentWave;
+    private int waveNum;
+    private int mode;
+    private Board board;
+    private Player player;
+    private int hp = 3;
+
+    public Game(int level, int mode, Board board, Player player){
+        this.level = level;
+        this.currentWave = 0;
+        this.board = board;
+        this.player = player;
+        this.waveNum = level + 3;
+        this.mode = mode;
         this.gameInterface = new GameInterface();
-        System.out.println("Game Starting");
+
+        this.startGame();
+        while (!this.gameOver() & !this.gameWon()){
+
+        }
     }
+
+    public boolean gameOver(){
+        return this.hp <= 0;
+    }
+    public boolean gameWon(){
+        return this.currentWave == this.waveNum;
+    }
+    public void startGame(){
+        System.out.println("Game Starting");
+        this.currentWave += 1;
+    }
+
 }
---
+++ src/com/li/oopproject/Menu.java
@@ -0,0 +1,17 @@
+package com.li.oopproject;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.File;
+import java.io.IOException;
+
+
+public class Menu extends JFrame {
+
+}
